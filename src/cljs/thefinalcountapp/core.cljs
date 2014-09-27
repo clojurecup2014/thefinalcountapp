eaff2092d015f5192e4ab5e3c085a25290113275
alonso.torres <alonso.torres@kaleidos.net>
2014-09-27 21:20:22 +0200

    First ingration

--- src/cljs/thefinalcountapp/core.cljs
+++ src/cljs/thefinalcountapp/core.cljs
@@ -6,7 +6,8 @@
             [cljs.core.async :refer [<!]]))
 
 (def state (atom {:group "kaleidos-team"
-                  :counters []}))
+                  :counters []
+                  :displaying 0}))
 
 ;; (defn main-component []
 ;;   [:div.content
@@ -20,6 +21,20 @@
 ;    [:g {:transform "translate(680, 300)"}
 ;     [:g {:transform "scale(1.0, 1.0)"} [counter]]]]])
 
+(defn counter-list []
+  (.log js/console (count (:counters @state)))
+  (if (>= (count (:counters @state)) 3)
+    (let [idx (:displaying @state)
+          center (nth (:counters @state) idx)
+          left-idx (if (< (dec idx) 0) (dec (count (:counters @state))) (dec idx))
+          left (nth (:counters @state) left-idx)
+          right-idx (if (> (inc idx) (dec (count (:counters @state)))) 0 (inc idx))
+          right (nth (:counters @state) right-idx)]
+      [:g
+       [:g {:transform "translate(350, 120) scale(0.8, 0.8)"} [counter "DÍAS SIN 1" (:text left) "0" "0" "1" "Último reset 18/09/2014 a las 9:00h" "0.4"]]
+       [:g {:transform "translate(1080, 120) scale(0.8, 0.8)"} [counter "DÍAS SIN 2" (:text right) "0" "0" "3" "Último reset 18/09/2014 a las 9:00h" "0.4"]]
+       [:g {:transform "translate(640, 80)"} [counter "DÍAS SIN 3" (:text center) "0" "0" "2" "Último reset 18/09/2014 a las 9:00h" "1.0"]]])))
+
 (defn main-component []
   [:div
    [:div
@@ -29,15 +44,13 @@
      [:a {:class "add-btn" :href "#"} "+"]]]
    [:div#content
     [:svg {:width "100%" :height "100%"}
-     [:g {:transform "translate(350, 120) scale(0.8, 0.8)"} [counter "DÍAS SIN 1" "que nos pierda la boca" "0" "0" "1" "Último reset 18/09/2014 a las 9:00h" "0.4"]]
-     [:g {:transform "translate(1080, 120) scale(0.8, 0.8)"} [counter "DÍAS SIN 2" "que nos pierda la boca" "0" "0" "3" "Último reset 18/09/2014 a las 9:00h" "0.4"]]
-     [:g {:transform "translate(640, 80)"} [counter "DÍAS SIN 3" "que nos pierda la boca" "0" "0" "2" "Último reset 18/09/2014 a las 9:00h" "1.0"]]
+     [counter-list]
      ]]])
 
 
 (defn ^:export run []
   (go (let [response (<! (http/get "/api/counters/kaleidos-team"))]
         (.log js/console (str response))
-        (swap! state #(assoc % :counters (:data response)))))
+        (swap! state #(assoc % :counters (-> response :data :counters)))))
   (reagent/render-component [main-component]
                             (.-body js/document)))
