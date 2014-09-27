d791f648d4342b6c5845c32b1147a6b1e2392c5f
Alejandro Gómez <alejandro@dialelo.com>
2014-09-27 09:27:44 +0000

    :baby:

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to thefinalcountapp
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject thefinalcountapp "0.1.0-SNAPSHOT"
+  :description "The Final Count App is an application for sharing your personal records with your team or friends. Have you ever been two full days without breaking your local git repository? Now you can brag about it!"
+  :url "http://thefinalcountapp.clojurecup.com"
+  :license {:name "GNU Affero General Public License"
+            :url "http://www.gnu.org/licenses/agpl-3.0.html"}
+  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]])
