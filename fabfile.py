b78dc8e8a1e988935c7094a79b668b2201b9bbcb
Alejandro Gómez <alejandro@dialelo.com>
2014-09-27 12:18:38 +0000

    Compile CLJS and run server on background

--- fabfile.py
+++ fabfile.py
@@ -15,6 +15,7 @@ PROJECT_DIR = "/home/cloudsigma/thefinalcountapp"
 def deploy():
     update_code()
     kill_server()
+    compile_cljs()
     run_server()
 
 def update_code():
@@ -22,9 +23,16 @@ def update_code():
         run("git reset --hard HEAD")
         run("git pull --rebase origin master")
 
+def compile_cljs():
+    with cd(PROJECT_DIR):
+        run("lein cljsbuild once")
+
 def kill_server():
     run("pkill java", warn_only=True)
 
 def run_server():
     with cd(PROJECT_DIR):
-        run("nohup lein run &")
+        run_bg("lein run")
+
+def run_bg(cmd):
+    run("dtach -n `mktemp -u /tmp/{socket}.XXXX` {cmd}".format(socket="dtach", cmd=cmd))
